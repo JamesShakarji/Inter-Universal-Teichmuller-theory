897bef01c5a964ea0e730d8f6f8fb9326377013a
James Shakarji <77446852+JamesShakarji@users.noreply.github.com>
2022-12-26 20:23:04 -0500

    Create IUT_Galois-Lesson_in_values_distances_periods_and_characters.py

---
+++ IUT_Galois-Lesson_in_values_distances_periods_and_characters.py
@@ -0,0 +1,42 @@
+from math import pi
+
+# Define a function that computes the "inter-universal Teichmuller character" of a Galois representation
+def inter_universal_teichmuller_character(Galois_rep):
+# Compute the sum of the values in the Galois representation
+character = sum(Galois_rep.values())
+return character
+
+# Define a function that computes the "inter-universal Teichmuller period" of a Galois representation
+def inter_universal_teichmuller_period(Galois_rep):
+# Compute the product of the values in the Galois representation
+period = 1
+for value in Galois_rep.values():
+period *= value
+return period
+
+# Define a function that computes the "inter-universal Teichmuller distance" between two Galois representations
+def inter_universal_teichmuller_distance(Galois_rep_1, Galois_rep_2):
+# Initialize the distance to zero
+distance = 0
+# Loop through the keys in both Galois representations
+for key in Galois_rep_1.keys():
+# Compute the difference between the values for the key in each representation
+value_difference = abs(Galois_rep_1[key] - Galois_rep_2[key])
+# Add the difference to the total distance
+distance += value_difference
+return distance
+
+# Example usage: Compute the inter-universal Teichmuller character of a Galois representation
+Galois_rep = {'a': 2*pi, 'b': 3*pi}  # Example Galois representation
+character = inter_universal_teichmuller_character(Galois_rep)
+print(f'Inter-universal Teichmuller character: {character}')
+
+# Example usage: Compute the inter-universal Teichmuller period of a Galois representation
+period = inter_universal_teichmuller_period(Galois_rep)
+print(f'Inter-universal Teichmuller period: {period}')
+
+# Example usage: Compute the inter-universal Teichmuller distance between two Galois representations
+Galois_rep_1 = {'a': 2*pi, 'b': 3*pi}  # Example Galois representation 1
+Galois_rep_2 = {'a': 3*pi, 'b': 4*pi}  # Example Galois representation 2
+distance = inter_universal_teichmuller_distance(Galois_rep_1, Galois_rep_2)
+print(f'Inter-universal Teichmuller distance: {distance}')
