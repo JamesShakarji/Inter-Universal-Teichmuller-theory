66c349c10a509ae7f4997551fdb9533980e6b218
James Shakarji <77446852+JamesShakarji@users.noreply.github.com>
2023-01-02 20:40:46 -0500

    Update L-function_of_IUT-Galois_representations.py

--- L-function_of_IUT-Galois_representations.py
+++ L-function_of_IUT-Galois_representations.py
@@ -1,6 +1,4 @@
-# takes as input an IUT Galois representation (given as a dictionary mapping element names to angles) and a complex number s, and returns the value of the 
-# L-function of the Galois representation at s. The L-function is computed by looping over the elements of the Galois representation and multiplying the current 
-# value of the L-function by the contribution of each element to the L-function. The contribution of an element is computed using the angle associated with the element.
+import math
 
 def L_function_IUT_Galois_rep(Galois_rep, s):
     """Computes the L-function of an inter-universal Teichmuller (IUT) Galois representation."""
@@ -9,13 +7,12 @@ def L_function_IUT_Galois_rep(Galois_rep, s):
     # Loop over the elements of the Galois representation
     for g, angle in Galois_rep.items():
         # Compute the contribution of this element to the L-function
-        L *= 1 / (1 - complex(cos(angle), sin(angle)) * s)
+        L *= 1 / (1 - complex(math.cos(angle), math.sin(angle)) * s)
     # Return the L-function value
     return L
 
 # Example usage: Compute the L-function of an IUT Galois representation
-Galois_rep = {'a': 2*pi, 'b': 3*pi}  # Example IUT Galois representation
+Galois_rep = {'a': 2*math.pi, 'b': 3*math.pi}  # Example IUT Galois representation
 s = complex(-1, 0.5)  # Example complex number
 L = L_function_IUT_Galois_rep(Galois_rep, s)
 print(f'L-function of IUT Galois representation: {L}')
-
