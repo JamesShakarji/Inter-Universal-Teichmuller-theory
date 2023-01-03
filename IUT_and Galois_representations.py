97dd5adc350697bcf02d34661ad03b9f07b5aa88
James Shakarji <77446852+JamesShakarji@users.noreply.github.com>
2023-01-02 20:45:35 -0500

    Update IUT_and Galois_representations.py

--- IUT_and Galois_representations.py
+++ IUT_and Galois_representations.py
@@ -2,30 +2,29 @@ from math import pi
 
 # Define a function that computes the "inter-universal Teichmuller character" of a Galois representation
 def inter_universal_teichmuller_character(Galois_rep):
-# Compute the sum of the values in the Galois representation
-character = sum(Galois_rep.values())
-return character
-#Galois_rep is defined in line 31, due to its relevance to the IUT character
+    # Compute the sum of the values in the Galois representation
+    character = sum(Galois_rep.values())
+    return character
 
 # Define a function that computes the "inter-universal Teichmuller period" of a Galois representation
 def inter_universal_teichmuller_period(Galois_rep):
-# Compute the product of the values in the Galois representation
-period = 1
-for value in Galois_rep.values():
-period *= value
-return period
+    # Compute the product of the values in the Galois representation
+    period = 1
+    for value in Galois_rep.values():
+        period *= value
+    return period
 
 # Define a function that computes the "inter-universal Teichmuller distance" between two Galois representations
 def inter_universal_teichmuller_distance(Galois_rep_1, Galois_rep_2):
-# Initialize the distance to zero
-distance = 0
-# Loop through the keys in both Galois representations
-for key in Galois_rep_1.keys():
-# Compute the difference between the values for the key in each representation
-value_difference = abs(Galois_rep_1[key] - Galois_rep_2[key])
-# Add the difference to the total distance
-distance += value_difference
-return distance
+    # Initialize the distance to zero
+    distance = 0
+    # Loop through the keys in both Galois representations
+    for key in Galois_rep_1.keys():
+        # Compute the difference between the values for the key in each representation
+        value_difference = abs(Galois_rep_1[key] - Galois_rep_2[key])
+        # Add the difference to the total distance
+        distance += value_difference
+    return distance
 
 # Example usage: Compute the inter-universal Teichmuller character of a Galois representation
 Galois_rep = {'a': 2*pi, 'b': 3*pi}  # Example Galois representation
@@ -41,3 +40,8 @@ Galois_rep_1 = {'a': 2*pi, 'b': 3*pi}  # Example Galois representation 1
 Galois_rep_2 = {'a': 3*pi, 'b': 4*pi}  # Example Galois representation 2
 distance = inter_universal_teichmuller_distance(Galois_rep_1, Galois_rep_2)
 print(f'Inter-universal Teichmuller distance: {distance}')
+
+#Example output
+#Inter-universal Teichmuller character: 15.707963267948966
+#Inter-universal Teichmuller period: 59.21762640653615
+#Inter-universal Teichmuller distance: 6.283185307179586
